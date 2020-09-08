14ca073d73b101deabb620ab03be73e87984aae2
Lee, Jun Seok <lego3410@gmail.com>
2020-09-09 01:21:36 +0900

    Add run arguments for pit.py (#3)

--- pit.py
+++ pit.py
@@ -7,11 +7,21 @@ from ataxx.pytorch.NNet import NNetWrapper as NNet
 
 import numpy as np
 from utils import *
+import argparse
 
 """
 use this script to play any two agents against each other, or play manually with
 any agent.
 """
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--model-file', default='./temp/best.pth.tar', metavar='PATH', help='Path to model file. Default is ./temp/best.pth.tar')
+# parser.add_argument('')
+a = parser.parse_args()
+
+path = a.model_file.split('/')
+model_dir = '/'.join(path[:-1])
+model_file = path[-1]
 
 human_vs_cpu = True
 
@@ -26,7 +36,7 @@ hp = HumanAtaxxPlayer(g).play
 
 # nnet players
 n1 = NNet(g)
-n1.load_checkpoint('./temp/', 'best.pth.tar')
+n1.load_checkpoint(model_dir, model_file)
 args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
 mcts1 = MCTS(g, n1, args1)
 n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
@@ -35,7 +45,7 @@ if human_vs_cpu:
     player2 = hp
 else:
     n2 = NNet(g)
-    n2.load_checkpoint('./temp/', 'best.pth.tar')
+    n2.load_checkpoint(model_dir, model_file)
     args2 = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
     mcts2 = MCTS(g, n2, args2)
     n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
