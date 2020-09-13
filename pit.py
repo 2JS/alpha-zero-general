77154f6ad3b89f3f3b3ec339d6d92ab128044399
Lee, Jun Seok <lego3410@gmail.com>
2020-09-13 21:05:45 +0900

    Add second player model file option (#3)

--- pit.py
+++ pit.py
@@ -18,6 +18,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--model-file', default='./temp/best.pth.tar', metavar='PATH', help='Path to model file. Default is ./temp/best.pth.tar')
 parser.add_argument('-m', '--mcts', default=300, type=int, metavar='N', help='Number of MCTS simulation per turn')
 parser.add_argument('-p', '--player', default='human', metavar='P', help='AI vs PLAYER. Default P is human', choices=['human', 'random', 'greedy', 'ai'])
+parser.add_argument('-g', '--model-file-p2', default='./temp/best.pth.tar', metavar='PATH', help='Path to model file of second ai player.')
 # parser.add_argument('')
 a = parser.parse_args()
 
@@ -25,6 +26,9 @@ path = a.model_file.split('/')
 model_dir = '/'.join(path[:-1])
 model_file = path[-1]
 
+path2 = a.model_file_p2.split('/')
+model_dir2 = '/'.join(path2[:-1])
+model_file2 = path2[-1]
 
 g = AtaxxGame(7)
 
@@ -51,7 +55,7 @@ elif a.player == 'greedy':
     player2 = gp
 else:
     n2 = NNet(g)
-    n2.load_checkpoint(model_dir, model_file)
+    n2.load_checkpoint(model_dir2, model_file2)
     args2 = dotdict({'numMCTSSims': a.mcts, 'cpuct': 1.0})
     mcts2 = MCTS(g, n2, args2)
     n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
